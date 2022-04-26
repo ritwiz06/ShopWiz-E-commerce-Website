86fe77b7e5db17ce4882c847aa99c2c3ec82e89b
ritwiz06 <ritik.agrawal06@gmail.com>
2022-04-26 15:20:53 +0530

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW CART ===============*/
+
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW LOGIN ===============*/
+
+
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== LIGHT BOX ===============*/
+
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+
+/*=============== STYLE SWITCHER ===============*/
