b978e40b350976e3319197ac4f40c3f37fc8ff4d
ritwiz06 <ritik.agrawal06@gmail.com>
2022-04-27 19:35:13 +0530

    Added Home page slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,15 +32,28 @@
 
 
 /*=============== HOME SWIPER ===============*/
+var swiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // when the scroll is greater than 50 viewport height, add the sroll header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 /*=============== NEW SWIPER ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== LIGHT BOX ===============*/
