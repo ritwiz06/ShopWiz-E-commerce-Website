c8843bb72523c2281b1216bedfd5af5119381f61
ritwiz06 <ritik.agrawal06@gmail.com>
2022-05-05 12:11:24 +0530

    Added steps section and newsletter

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,9 @@ var homeSwiper = new Swiper(".home-swiper", {
 function scrollHeader() {
     const header = document.getElementById('header')
     // when the scroll is greater than 50 viewport height, add the sroll header class to the header tag
-    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 50)
+        header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 /*=============== NEW SWIPER ===============*/
