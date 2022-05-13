6c8204269f4dfa092e06f6b7eb88925d71374c2a
ritwiz06 <ritik.agrawal06@gmail.com>
2022-05-14 01:12:21 +0530

    HomePage Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,66 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== SHOW CART ===============*/
-
+const cart = document.getElementById('cart'),
+    cartShop = document.getElementById('cart-shop'),
+    cartClose = document.getElementById('cart-close')
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
+if (cartShop) {
+    cartShop.addEventListener("click", () => {
+        cart.classList.add('show-cart')
+    })
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if (cartClose) {
+    cartClose.addEventListener("click", () => {
+        cart.classList.remove('show-cart')
+    })
+}
 
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById('login'),
+    loginButton = document.getElementById('login-button'),
+    loginClose = document.getElementById('login-close')
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
+if (loginButton) {
+    loginButton.addEventListener("click", () => {
+        login.classList.add('show-login')
+    })
+}
 
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if (loginClose) {
+    loginClose.addEventListener("click", () => {
+        login.classList.remove('show-login')
+    })
+}
 
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
@@ -61,7 +92,12 @@ var newSwiper = new Swiper(".new-swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+    //when scroll is higehr than 350 viewport height, add the schow scroll class to a tag with the scroll-top class
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== LIGHT BOX ===============*/
 
