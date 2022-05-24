46e3e8f4b7db59d1f05b38b667e7003ed76e16a8
ritwiz06 <ritik.agrawal06@gmail.com>
2022-05-24 11:25:29 +0530

    Navbar Pages Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,6 +103,34 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItem = document.querySelectorAll('.questions__item')
 
+accordionItem.forEach((item) => {
+    const accordionHeader = item.querySelector('.questions__header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.questions__content')
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
 
 /*=============== STYLE SWITCHER ===============*/
